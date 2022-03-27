ab022191a21ebee60e737b6ace1ceb7dd2e523f3
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-03-27 22:41:50 +0400

    реализовал переход к блоку после нажатия на пункт меню

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import modal from './modules/modal';
 import accordeon from './modules/accordeon';
 import serviceSlider from './modules/serviceSlider';
 import smoothScroll from './modules/smoothScroll';
+import menu from './modules/menu';
 
 modal();
 accordeon();
 serviceSlider();
 smoothScroll();
+menu();
 
 
