3a48959f3a21961a5d3b972b7b82a34b3467e288
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-03-27 20:29:36 +0400

    реализован плавный скролл по клику на стрелку

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import modal from './modules/modal';
 import accordeon from './modules/accordeon';
 import serviceSlider from './modules/serviceSlider';
+import smoothScroll from './modules/smoothScroll';
 
 modal();
 accordeon();
 serviceSlider();
+smoothScroll();
 
 
---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,26 @@
+const smoothScroll = () => {
+	const currentSection = document.querySelector('.push70');
+	const up = document.querySelector('.up');
+	const upToTop = document.querySelector('.up_to_top');
+
+	window.addEventListener('scroll', () => {
+		const top = document.documentElement.scrollTop;
+		if (top < currentSection.offsetTop) {
+			up.style.display = 'none';
+		} else if (top > currentSection.offsetTop) {
+			up.style.display = 'block';
+		}
+	});
+	upToTop.addEventListener('click', (e) => {
+		e.preventDefault();
+		const idTop = upToTop.getAttribute('href');
+		console.log(idTop)
+
+		document.querySelector(idTop).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});
+	});
+};
+
+export default smoothScroll;
