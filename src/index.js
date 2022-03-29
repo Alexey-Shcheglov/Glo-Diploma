27359eadbf2add061eeedd302ef43cac387d4ddd
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-03-29 14:11:22 +0400

    created slider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import accordeon from './modules/accordeon';
 import serviceSlider from './modules/serviceSlider';
 import smoothScroll from './modules/smoothScroll';
 import menu from './modules/menu';
+import slider from './modules/slider';
+
 
 modal();
 accordeon();
 serviceSlider();
 smoothScroll();
 menu();
+slider();
 
 
