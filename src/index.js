54105cf0bca7f350611ebd37339252599ca4b36c
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-03-29 21:41:13 +0400

    finish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import modalWindow from './modules/modalWindow';
 import menuList from './modules/menuList';
 import mobileMenu from './modules/mobileMenu';
-import link from './modules/link';
 import validation from './modules/validation';
 import topSlider from './modules/topSlider';
 import faq from './modules/faq';
@@ -13,7 +12,6 @@ import carousel from './modules/carousel';
 modalWindow();
 menuList();
 mobileMenu();
-link();
 validation();
 topSlider();
 faq();
